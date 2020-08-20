4edac8d4c90e49acaad4b34ad453bec5f16b3b84
doodlezucc <electerminator@gmail.com>
2020-08-20 02:39:58 +0200

    only display setup on first install

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.create({ url: "https://music.youtube.com" }, function(tab) {
-        chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
-            if (info.status === "complete" && tabId === tab.id) {
-                chrome.tabs.onUpdated.removeListener(listener);
-                chrome.tabs.sendMessage(tab.id, { action: "auto_extract" });
-            }
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason === "install") {
+        chrome.tabs.create({ url: "https://music.youtube.com" }, function(tab) {
+            chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+                if (info.status === "complete" && tabId === tab.id) {
+                    chrome.tabs.onUpdated.removeListener(listener);
+                    chrome.tabs.sendMessage(tab.id, { action: "auto_extract" });
+                }
+            });
         });
-    });
+    }
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, _info, tab) {
-    console.log(tab);
     if (tab.status === "complete" && tab.url.includes(".youtube.com/")) {
         chrome.pageAction.show(tabId);
     } else {
