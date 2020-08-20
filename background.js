01dc67e4b62004d9a821bb43f07be68ff9c6d5e8
doodlezucc <electerminator@gmail.com>
2020-08-20 02:16:48 +0200

    firefox support

--- background.js
+++ background.js
@@ -1,18 +1,19 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostSuffix: '.youtube.com' },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
     chrome.tabs.create({ url: "https://music.youtube.com" }, function(tab) {
         chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
-            if (info.status === 'complete' && tabId === tab.id) {
+            if (info.status === "complete" && tabId === tab.id) {
                 chrome.tabs.onUpdated.removeListener(listener);
                 chrome.tabs.sendMessage(tab.id, { action: "auto_extract" });
             }
         });
     });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, _info, tab) {
+    console.log(tab);
+    if (tab.status === "complete" && tab.url.includes(".youtube.com/")) {
+        chrome.pageAction.show(tabId);
+    } else {
+        chrome.pageAction.hide(tabId);
+    }
 });
